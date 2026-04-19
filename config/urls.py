48c9c6abebe549f1769dec2883dd6a958675bd2c
planeboom <3211742933@qq.com>
2026-04-19 15:57:58 +0800

    4. add swagger

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,20 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from api.views import BeverageViewSet
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Starbucks Nutrition API",
+        default_version='v1',
+        description="Web Services Coursework 1 API Documentation",
+        contact=openapi.Contact(email="your_email@leeds.ac.uk"), # 可以换成你的邮箱
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 router = DefaultRouter()
 router.register(r'beverages', BeverageViewSet)
@@ -9,4 +23,6 @@ router.register(r'beverages', BeverageViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
